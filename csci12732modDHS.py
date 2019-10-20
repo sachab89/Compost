b7dd4c22e3b076dbeb758a1085592fa32c375c5a
sachab89 <56741088+sachab89@users.noreply.github.com>
2019-10-20 14:17:18 -0400

    Add files via upload

---
+++ csci12732modDHS.py
@@ -0,0 +1,23 @@
+#Name: Shane Bastien
+#Email: sachabastien@gmail.com
+#Date: October 20, 2019
+
+#ask the user to specify the input file,
+#ask the user to specify the output file,
+#make a plot of the fraction of the total population that are children over time from the data in input file, and
+#store the plot in the output file the user specified.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+homeless = pd.read_csv(input("please enter name of file:"))
+outfile= input("please enter name of output file:")
+homeless["frac"]= homeless["Total Children in Shelter"]/homeless["Total Individuals in Shelter"]
+                                                                           
+homeless.plot(x= "Date of Census", y= "frac")
+plt.show()
+
+fig= plt.gcf()
+fig.savefig(outfile)
